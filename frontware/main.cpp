96dfe6fb79cd36860ce26d1447e6ea54891495f7
mXn <legeardflavian291@gmail.com>
2024-03-11 23:39:35 +0100

    avoid multiple instance and hide console

--- frontware/main.cpp
+++ frontware/main.cpp
@@ -1,10 +1,25 @@
 #include "systeme.h"
 #include <iostream>
+#include <thread>
+#include "headers.h"
 
 int main(void) {
 
+#if DEBUG
+	std::cout << "DEBUG MODE" << std::endl;
+#else
+	::ShowWindow(::GetConsoleWindow(), SW_HIDE);
+#endif
+
+	HANDLE hMutex = CreateMutex(NULL, TRUE, L"b29wc2llIHRoYXRzIHNhZCBmb3IgeW91IF5e=");
+	if (ERROR_ALREADY_EXISTS == GetLastError()) {
+		return 1;
+	}
+
+
 
 	Systeme sys;
+
 
 	
 	return 0;
