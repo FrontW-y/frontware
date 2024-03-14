21790a1ca6686be0a7febb44961d7f75eab7ab74
mXn <legeardflavian291@gmail.com>
2024-03-14 15:04:41 +0100

    change on methode

--- frontware/fileEncryptor.h
+++ frontware/fileEncryptor.h
@@ -1,27 +1,17 @@
 #include <string>
 
 
-#pragma comment(lib, "crypt32.lib")
+
 
 class FileEncryptor
 {
 
 private:
-	std::string _disk;
-	std::string _rsaKey;
-	std::string _aesKey;
+	
 
 public:
 
-	FileEncryptor(std::string disk, std::string aesKey, std::string rsaKey);
-	~FileEncryptor();
-
-	bool aesEncryptFile(const char* file, std::string& aesKey);
-	bool rsaEncryptFile(const char* file, std::string& rsaKey);
-
-	bool setRsaKey(std::string rsaKey);
-	bool setAesKey(std::string aesKey);
-	bool setDisk(std::string disk);
+	
 	
 
 };
